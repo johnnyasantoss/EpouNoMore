1a734938cc03ce34340f7ae0139628b906bd9c28
Johnny Santos <johnnyadsantos@gmail.com>
2019-07-24 17:06:15 +0200

    Add todo to cli

--- src/EpouNoMore.CLI/Program.cs
+++ src/EpouNoMore.CLI/Program.cs
@@ -4,9 +4,9 @@ namespace EpouNoMore.CLI
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("TODO: Use UI for now");
         }
     }
 }
